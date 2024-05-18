edd62fe3a71feb0af3edf9e37be27d2096645da3
Cat-In-Case <resource333@gmail.com>
2024-05-19 05:42:57 +0900

    Parser and Example Add

---
+++ Script/BaseJsonParser.cs
@@ -0,0 +1,120 @@
+#region Using
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Linq;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Utilities;
+#endregion
+
+//Read Json on Runtime
+public abstract class BaseJsonParser<DICTIONARY, CLASS> : IDisposable where DICTIONARY : BaseJsonDictionary<CLASS>,  new()
+{
+    protected const string DATA_KEY_STRING = "data";
+    protected BaseJsonParser()    {    }
+    protected JObject root; protected JObject data;
+    [SerializeField] protected string json { get; set; }
+    public virtual async Task<DICTIONARY> Read()
+    {
+        await Task.Delay(500);
+        return  null;
+    }
+    public abstract Task<DICTIONARY> ToDictionary(JArray array);
+    public virtual void Dispose()    {    }
+}
+
+
+#region File
+public class AsyncFileIO : IDisposable
+{
+    public async Task<bool> FileSave(string path, string fileName, string jsonData)
+    {
+
+        using (FileStream fs = new FileStream(string.Format("{0}/{1}", path, fileName), FileMode.Create))
+        {
+            var t = Task<string>.Run(async () =>
+            {
+                byte[] data = Encoding.UTF8.GetBytes(jsonData);
+                fs.Write(data, 0, data.Length);
+            });
+
+            await t;
+#if UNITY_EDITOR
+            Debug.Log(Thread.CurrentThread.ManagedThreadId);
+#endif
+            if (t.IsFaulted == true)
+            {
+                return false;
+            }
+            else
+            {
+                if (t.IsCompletedSuccessfully == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+    public async Task<string> FileLoad(string path, string fileName)
+    {
+        string jsonText = "";
+        using (FileStream fs = new FileStream(string.Format("{0}/{1}", path, fileName), FileMode.Open))
+        {
+            var t = Task<string>.Run(async () =>
+            {
+                byte[] data = new byte[fs.Length];
+                fs.Read(data, 0, data.Length);
+                jsonText = Encoding.UTF8.GetString(data);
+            });
+            await t;
+
+            if (t.IsFaulted == true)
+            {
+                Console.WriteLine(t.Exception.Message);
+                jsonText = "Error";
+            }
+        }
+#if UNITY_EDITOR
+        Debug.Log(Thread.CurrentThread.ManagedThreadId);
+#endif
+        return jsonText;
+    }
+
+    public Task<bool> FileExist(string path, string fileName)
+    {
+        if (File.Exists(string.Format("{0}/{1}", path, fileName)) == true)
+        {
+#if UNITY_EDITOR
+            Debug.Log(Thread.CurrentThread.ManagedThreadId);
+#endif
+            return Task.FromResult(true);
+        }
+        else
+        {
+#if UNITY_EDITOR
+            Debug.Log(Thread.CurrentThread.ManagedThreadId);
+#endif
+            return Task.FromResult(false);
+        }
+    }
+
+
+
+    public void Dispose()
+    {
+
+    }
+}
+#endregion
---
+++ Script/Example/ItemParser.cs
@@ -0,0 +1,73 @@
+#region Using
+using System.Collections;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Utilities;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CustomDictionary.SerializableDictionary;
+#endregion
+
+public class ItemParser : BaseJsonParser<ItemJsonDic, ItemJsonData>
+{
+    private const string ITEM_KEY_STRING = "items";
+    private JArray items;
+    [SerializeField] private bool rootLoaded = false;
+    public ItemParser(string jsonData)
+    {
+        json = jsonData;
+        rootLoaded = false;
+    }
+    public ItemParser(ref JObject root, ref JObject data)
+    {
+        this.root = root;
+        this.data = data;
+        rootLoaded = true;
+    }
+    public override async Task<ItemJsonDic> Read()
+    {
+        if(rootLoaded == false)
+        {
+            root = JObject.Parse(json);
+
+            if (data.ContainsKey(DATA_KEY_STRING) == false)
+                return await Task.FromException<ItemJsonDic>(new Exception("root/data is broken"));
+            data = root[DATA_KEY_STRING] as JObject;
+        }
+
+        if (data.ContainsKey(ITEM_KEY_STRING) == false)
+            return await Task.FromException<ItemJsonDic>(new Exception("root/data/items is broken"));
+        items = data[ITEM_KEY_STRING] as JArray;
+
+        ItemJsonDic block;
+        Task<ItemJsonDic> itemTask = Task<ItemJsonDic>.Run(() => ToDictionary(items));
+        await itemTask;
+
+        if (itemTask.IsFaulted == false)
+            return block = itemTask.Result;
+        else
+            return await Task.FromException<ItemJsonDic>(new Exception("Dictionary convert is faulted"));
+    }
+    public override Task<ItemJsonDic> ToDictionary(JArray array)
+    {
+        ItemJsonDic block = new ItemJsonDic();
+        block.FromJArray(ref array);
+        return Task.FromResult(block);
+    }
+    public override void Dispose()
+    {
+        root.RemoveAll();
+        data.RemoveAll();
+        items.Clear();
+        root = null;
+        data = null;
+        items = null;
+        json = "";
+        json = null;
+    }
+}
